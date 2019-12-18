9157e04f7fe1c7a8347cbb46078a97c807b64b41
Luigi Luz <lfml@cesar.org.br>
2019-12-18 10:20:52 -0300

    Work in progress
    Getting MPU Fault when calling spi_write function
    
    Signed-off-by: Luigi Luz <lfml@cesar.org.br>

--- samples/basic/spi/src/main.c
+++ samples/basic/spi/src/main.c
@@ -9,37 +9,70 @@
 #include <device.h>
 #include <stdio.h>
 #include <spi.h>
+#include <gpio.h>
+
+#define SS_PIN 22
 
 /* String que enviarei via SPI */
 
+u8_t byte_enviado = 1;
 char myString[] = "S";
 
 struct spi_buf bufs[] = {
 	{
-		.buf = myString,
-		.len = sizeof(myString)
+		.buf = &byte_enviado,
+		.len = sizeof(byte_enviado)
 	}
 };
 
-struct spi_buf_set tx = {
-	.buffers = bufs
+const struct spi_buf_set tx = {
+	.buffers = bufs,
+	.count = 1U,
 };
 
 void main(void)
 {
 	struct device *spi;
 	struct spi_config spi_cfg;
+	struct device *gpio_ss;		/* GPIO device */
 
 	printk("SPI communication example \n");
+
+	gpio_ss = device_get_binding(DT_NORDIC_NRF_GPIO_0_LABEL);
+	if (!gpio_ss) {
+		printk("Could not find GPIO driver \n");
+		return;
+	}
+	printk("blablabla1 \n");
+
+	gpio_pin_write(gpio_ss, SS_PIN, 0); /* Returns 0 if successfull */
+	printk("blablabla2 \n");
+	if (gpio_pin_write(gpio_ss, SS_PIN, 0)) {
+		printk("Error in gpio pin write, errno: %d \n", gpio_pin_write(gpio_ss, SS_PIN, 0));
+		return;
+	}
 
 	spi = device_get_binding(DT_SPI_2_NAME);
 	if (!spi) {
 		printk("Could not find SPI driver \n");
 		return;
 	}
-	spi_cfg.operation = SPI_WORD_SET(8);
-	spi_cfg.frequency = 256000U;
-	spi_cfg.slave = 0x02;
+	printk("blablabla3 \n");
 
-	spi_write(spi, &spi_cfg, &tx);
+	spi_cfg.operation = SPI_WORD_SET(8);
+	spi_cfg.frequency = 2000000U; /* Posso voltar pra 2000000 depois */
+
+	printk("blablabla4 \n");
+
+	/* Problema provavelmente está na passagem de parâmetros pro spi_write */
+
+	int error_spi = spi_write(spi, &spi_cfg, &tx);
+
+	/* if (error_spi < 0) {
+		printk("SPI driver error, errno:%d \n", error_spi);
+		return;
+	}
+	*/
+
+	printk("blablabla5 \n");
 }
